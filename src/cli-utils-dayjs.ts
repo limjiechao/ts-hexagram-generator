b770cc3110ab25e29a94853410c43ed4ced944c6
Lim Jiechao <jiechao.lim@gmail.com>
2025-06-11 11:05:53 +0800

    use dayjs for timestamp in file name

---
+++ src/cli-utils-dayjs.ts
@@ -0,0 +1,16 @@
+import dayjs from 'dayjs'
+import timezone from 'dayjs/plugin/timezone.js'
+import utc from 'dayjs/plugin/utc.js'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+/**
+ * Generate a filesystem-safe, timezone-aware timestamp string
+ * Format: YYYY-MM-DD_HH-mm-ss_ZZ (e.g., 2023-12-07_14-30-25_+0800)
+ *
+ * @returns A timestamp string safe for use in filenames
+ */
+export function getFilesystemSafeTimestamp(): string {
+  return dayjs().format('YYYY-MM-DD_HH-mm-ss_ZZ')
+}
